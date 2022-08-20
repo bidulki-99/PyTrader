5c00df1945e52407042386817794133971017a2c
wjddbs1509 <108559801+wjddbs1509@users.noreply.github.com>
2022-08-20 17:36:54 +0900

    Update Chapter18.3-Day3 Kiwoom.py

--- Chapter18.3-Day3 Kiwoom.py
+++ Chapter18.3-Day3 Kiwoom.py
@@ -1,1 +1,11 @@
+def _opw00001(self, rqname, trcode):
+    self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
+    
+#_receive_tr_data 메서드에서 _opw00001 메서드를 호출하도록 코드를 수정합니다.
+if rqname == "opt10081_req":
+    self._opt10081(rqname, trcode)
 
+elif rqname == "opw00001_req":
+    self._opw00001(rqname, trcode)
+    
+    
